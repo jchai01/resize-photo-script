287cdfaf642b62268455eceddffdc52f02a42595
Joseph Chai <josephchai100@hotmail.com>
2024-01-04 00:18:33 +0000

    add file

---
+++ convert.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# This script requires the download imagemagick.
+
+image_dir=$1 # First argument
+target_ratio="1280x960"
+
+if [ -z $image_dir ]; then
+    echo "Please provide image directory as first argument."
+    exit 1
+fi
+
+if [ ! -d $image_dir ]; then
+    echo "Not a valid directory"
+    exit 1
+fi
+
+output_dir="$image_dir-output"
+
+echo "Located $image_dir"
+echo "Creating output $output_dir"
+mkdir $output_dir
+
+for image in $image_dir/*; do
+    echo "Processing $image"
+    file_name=$(basename $image)
+
+    # convert -bordercolor white -border 100 -gravity center -resize 1200x1200 -extent 1200x1200 $image $output_dir/$file_name
+    convert -background black -gravity center -resize $target_ratio -extent $target_ratio $image $output_dir/$file_name
+
+done
